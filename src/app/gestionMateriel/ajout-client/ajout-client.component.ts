19bd9d9e58c3b19277df93caeedd8f2a839785dc
maxime <17.laurinmaxime@gmail.com>
2019-08-29 16:27:14 +0200

    Ajout client et liste ville

--- src/app/gestionMateriel/ajout-client/ajout-client.component.ts
+++ src/app/gestionMateriel/ajout-client/ajout-client.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VilleService } from '../ville.service';
+import { Ville } from '../ville';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ajout-client',
   templateUrl: './ajout-client.component.html',
-  styleUrls: ['./ajout-client.component.css']
+  styleUrls: ['./ajout-client.component.css'],
+  providers: [VilleService]
 })
 export class AjoutClientComponent implements OnInit {
   ajoutForm: FormGroup;
   submitted = false;
+  villes : Ville[] = null;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private router: Router, private villeService: VilleService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.getAllVilles();
     this.ajoutForm = this.formBuilder.group({
       nom: ['', Validators.required],
-      adresse1 : ['', Validators.required],
-      adresse2: ['', Validators.required],
-      ville: ['', Validators.required],
+      adresse1 : [''],
+      adresse2: [''],
+      ville: [''],
   }, {
 
   });
@@ -33,6 +39,8 @@ export class AjoutClientComponent implements OnInit {
           return;
       }
 
+      console.log(this.ajoutForm.get("nom"));
+
       // display form values on success
       alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.ajoutForm.value, null, 4));
   }
@@ -41,4 +49,10 @@ export class AjoutClientComponent implements OnInit {
       this.submitted = false;
       this.ajoutForm.reset();
   }
+  getAllVilles() {
+    this.villeService.getAllVilles().subscribe(data => this.villes = data);
+  }
+  
+
+  
 }
--- src/app/gestionMateriel/ville.ts
+++ src/app/gestionMateriel/ville.ts
@@ -2,14 +2,14 @@ export class Ville {
     public constructor (
         public id: number, 
         public codePostal: string, 
-        public ville: string) {}
+        public nom: string) {}
 
     public static fromJson(json: Object): 
     Ville {
         return new Ville(
             json['id'], 
             json['codePostal'],
-            json['ville']
+            json['nom']
         );
     }
     }
