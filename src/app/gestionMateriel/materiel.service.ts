2bda0de03064e20a6cbf3fa042ee7f69f32d721d
EtiennePS <etienne.picherit@gmail.com>
2019-08-29 14:16:24 +0200

    liste matériel

---
+++ src/app/gestionMateriel/materiel.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Materiel } from './materiel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaterielService {
+  baseUrl = "https://api-gestion-materiel.herokuapp.com/api/v1";
+  httpOptions = {
+    headers : new HttpHeaders({
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+      'Accept':'application/json',
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getMaterielsFromClient(idClient:number): Observable<Materiel[]> {
+    return this.http.get<Materiel[]>(this.baseUrl + '/clients/' + idClient + '/materiel', this.httpOptions).pipe();
+  }
+}
